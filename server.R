841838681cca81c162c6700ac43487b3445f8f89
flynn-d <34866670+flynn-d@users.noreply.github.com>
2021-05-25 21:15:39 -0400

    Updates for 2021 rates
    - Fixed all links, too

--- server.R
+++ server.R
@@ -10,12 +10,12 @@ rps_yr = rps_frame[rps_frame$Year == format(Sys.Date(), "%Y"),"RPS"]
 
 # test values
 
-  # input = list(custcharge = 6.43,
-  #              co2avoid = 0.857,
-  #              deliverycharges = 0.09824,
-  #             kwhslider = 450,
-  #             rate5 = 0.10538,
-  #             rate100 = 0.13198)
+# input = list(custcharge = 6.43,
+#              co2avoid = 0.857,
+#              deliverycharges = 0.09824,
+#             kwhslider = 450,
+#             rate5 = 0.10538,
+#             rate100 = 0.13198)
 
 
 server <- function(input, output, session) {
@@ -56,22 +56,22 @@ server <- function(input, output, session) {
   output$plot1 <- renderPlot({
     par(mar = c(6, 5, 4, 2))
     plotcols <- c("palegreen3","deepskyblue3",
-                  "palegreen4", "deepskyblue4")
+                  "palegreen3", "deepskyblue3")
     
-    bill5 <- input$custcharge + input$deliverycharges * input$kwhslider + input$rate5 * input$kwhslider
+    bill10 <- input$custcharge + input$deliverycharges * input$kwhslider + input$rate5 * input$kwhslider
     bill100 <- input$custcharge + input$deliverycharges * input$kwhslider + input$rate100 * input$kwhslider
-    co2_5 <- input$kwhslider * (rps_yr+5)/100 * input$co2avoid 
+    co2_10 <- input$kwhslider * (rps_yr+5)/100 * input$co2avoid 
     co2_100 <- input$kwhslider * input$co2avoid
     
-    plotdat <- c(bill5, -co2_5, bill100, -co2_100)
+    plotdat <- c(bill10, -co2_10, bill100, -co2_100)
     par(xpd=TRUE, cex = 1.25)
-
-    bp <- barplot(matrix(plotdat, nrow= 2), beside= T,
-            ylim = c(roundUp(1.1*min(plotdat)), roundUp(1.1*max(plotdat))),
-            yaxt = "n",
-            xlab = "Rates",
-            col = plotcols,
-            main = "Typical Monthly Bill and CO[2] Avoided"
+    
+    bp <- barplot(matrix(plotdat, nrow = 2), beside= T,
+                  ylim = c(roundUp(1.1*min(plotdat)), roundUp(1.1*max(plotdat))),
+                  yaxt = "n",
+                  xlab = '',#"Rates",
+                  col = plotcols,
+                  main = "Typical Monthly Bill and CO[2] Avoided"
     )
     axis(2, at = seq(0, roundUp(1.1*max(plotdat)), by = 100),
          labels = seq(0, roundUp(1.1*max(plotdat)), by = 100))
@@ -79,8 +79,8 @@ server <- function(input, output, session) {
           cex = 1.25, at = median(seq(0, roundUp(1.1*max(plotdat)), by = 100))+50)
     axis(2, at = seq(-100, roundUp(0.9*min(plotdat)), by = -100),
          labels = seq(100, -roundUp(0.9*min(plotdat)), by = 100))
-   
-     mtext("CO[2] avoided \n (lbs)", side = 2, line = 2.5,
+    
+    mtext("CO[2] avoided \n (lbs)", side = 2, line = 2.5,
           cex = 1.25, at = median(seq(-100, roundUp(0.9*min(plotdat)), by = -100))-50)
     
     text(x = bp[1,], y = plotdat[c(1,3)] + max(plotdat[c(1,3)]) * 0.25,
@@ -97,27 +97,27 @@ server <- function(input, output, session) {
          line = 3,
          lwd = 0,
          labels = paste(
-           c("+5% Renewable \n", 
+           c("+10% Renewable \n", 
              "100% Renewable \n"
              #,"Previous \n "
-             ),
+           ),
            c(round(100*input$rate5, 1),
              round(100*input$rate100, 1)
              #,round(100*input$rateprev, 1)
-             ),
+           ),
            "¢")
     )
-    }) # end plot1
+  }) # end plot1
   
   # other option: two plots, one for $ and one for CO2
   output$plot0 <- renderPlot({
     
     plotcols <- c("palegreen3", "palegreen4")
     
-    bill5 <- input$custcharge + input$deliverycharges * input$kwhslider + input$rate5 * input$kwhslider
+    bill10 <- input$custcharge + input$deliverycharges * input$kwhslider + input$rate5 * input$kwhslider
     bill100 <- input$custcharge + input$deliverycharges * input$kwhslider + input$rate100 * input$kwhslider
-
-    plotdat <- c(bill5, bill100)
+    
+    plotdat <- c(bill10, bill100)
     par(xpd=TRUE, cex = 1.25)
     
     bp <- barplot(plotdat,
@@ -131,7 +131,7 @@ server <- function(input, output, session) {
          labels = seq(0, roundUp(1.1*max(plotdat)), by = 100))
     mtext("Monthly bill ($)", side = 2, line = 2.5,
           cex = 1.25, at = median(seq(0, roundUp(1.1*max(plotdat)), by = 100)))
-
+    
     text(x = bp, y = plotdat + max(plotdat) * 0.25,
          labels = paste("$", format(
            round(plotdat, 2),
@@ -141,7 +141,7 @@ server <- function(input, output, session) {
     axis(1, at = bp, 
          lwd = 0,
          labels = paste(
-           c("+5% Renewable \n", 
+           c("+10% Renewable \n", 
              "100% Renewable \n"
              #,"Previous \n "
            ),
@@ -152,81 +152,80 @@ server <- function(input, output, session) {
            "¢")
     )
   }) # end plot0
-    
+  
   output$plot2 <- renderPlot({
-      plotcols2 <- c("deepskyblue3","deepskyblue4")
-      
-      co2.5 <- input$co2avoid * input$kwhslider * (rps_yr+5)/100 
-      co2.100 <- input$co2avoid * input$kwhslider
-      plotdat2 <- c(co2.5, co2.100)
-      par(xpd=TRUE, cex = 1.25)
-      
-      bp2 <- barplot(plotdat2,
-                    ylim = c(0, roundUp(1.1*max(plotdat2))),
-                    ylab = "Pounds of CO[2]",
-                    xlab = "Rates",
-                    col = plotcols2,
-                    main = "Emissions avoided per month")
-      
-      text(x = bp2, y = plotdat2 + max(plotdat2) * 0.15,
-           labels = paste(round(plotdat2, 1), "lbs"),
-           cex = 1.5, font = 2)
-      
-      axis(1, at = bp2, 
-           lwd = 0,
-           labels = paste(
-             c("+5% Renewable \n", 
-               "100% Renewable \n"
-             ),
-             c(round(100*input$rate5, 1),
-               round(100*input$rate100, 1)
-             ),
-             "¢")
-      )
+    plotcols2 <- c("deepskyblue3","deepskyblue4")
+    
+    co2.5 <- input$co2avoid * input$kwhslider * (rps_yr+5)/100 
+    co2.100 <- input$co2avoid * input$kwhslider
+    plotdat2 <- c(co2.5, co2.100)
+    par(xpd=TRUE, cex = 1.25)
+    
+    bp2 <- barplot(plotdat2,
+                   ylim = c(0, roundUp(1.1*max(plotdat2))),
+                   ylab = "Pounds of CO[2]",
+                   xlab = "Rates",
+                   col = plotcols2,
+                   main = "Emissions avoided per month")
+    
+    text(x = bp2, y = plotdat2 + max(plotdat2) * 0.15,
+         labels = paste(round(plotdat2, 1), "lbs"),
+         cex = 1.5, font = 2)
+    
+    axis(1, at = bp2, 
+         lwd = 0,
+         labels = paste(
+           c("+10% Renewable \n", 
+             "100% Renewable \n"
+           ),
+           c(round(100*input$rate5, 1),
+             round(100*input$rate100, 1)
+           ),
+           "¢")
+    )
   })
   
   output$dollartext <- renderText({
-    bill5 <- input$custcharge + input$deliverycharges * input$kwhslider + input$rate5 * input$kwhslider
+    bill10 <- input$custcharge + input$deliverycharges * input$kwhslider + input$rate5 * input$kwhslider
     bill100 <- input$custcharge + input$deliverycharges * input$kwhslider + input$rate100 * input$kwhslider
     
     paste("Your typical monthly bill would be <b>$",
-          format(round(bill100 - bill5, 2), nsmall = 2), 
+          format(round(bill100 - bill10, 2), nsmall = 2), 
           "</b> greater if you choose the 100% renewable option (Somerville 100% Local Green)",
           sep = "")
     
   })
   
   output$pcttext <- renderText({
-    bill5 <- input$custcharge + input$deliverycharges * input$kwhslider + input$rate5 * input$kwhslider
+    bill10 <- input$custcharge + input$deliverycharges * input$kwhslider + input$rate5 * input$kwhslider
     bill100 <- input$custcharge + input$deliverycharges * input$kwhslider + input$rate100 * input$kwhslider
     
-    r1 <- bill100/bill5
+    r1 <- bill100/bill10
     r2 <- r1-1
     paste("This would be a <b>",
           round(100*(r2), 1),
-          "%</b> increase over the +5% renewable option (Somerville Local Green)",
+          "%</b> increase over the +10% renewable option (Somerville Local Green)",
           sep = "")
     
   })
-
+  
   output$co2avoid <- renderText({
-    co2_5 <- input$kwhslider * (rps_yr+5)/100 * input$co2avoid # lbs of CO2 per kWh of renewable energy
+    co2_10 <- input$kwhslider * (rps_yr+5)/100 * input$co2avoid # lbs of CO2 per kWh of renewable energy
     co2_100 <- input$kwhslider * input$co2avoid
     
-    r1 <- co2_100/co2_5
+    r1 <- co2_100/co2_10
     r2 <- r1-1
     
-    r3 <- co2_100-co2_5
+    r3 <- co2_100-co2_10
     
     paste("This would be a <b>",
           round(r2, 1),
-          "x</b> decrease in CO2 emissions compared to the +5% renewable option. </br>",
-    paste("This is equivalent to removing an additional <b>",
+          "x</b> decrease in CO2 emissions compared to the +10% renewable option. </br>",
+          paste("This is equivalent to removing an additional <b>",
                 round(r3*12/1000, 1),
                 "</b> tons of CO2 per year, just for your household"),      
           sep = "")
     
   })
-
-  }
-
+  
+}
--- ui.R
+++ ui.R
@@ -36,76 +36,76 @@ ui <- dashboardPage(skin = "black",
                       tags$style(HTML(".irs-max {font-family: 'arial'; color: black;}")),
                       tags$style(HTML(".irs-min {font-family: 'Segoe UI', 'Source Sans Pro', Calibri, Candara, Arial, sans-serif; color: black;}")),
                       tags$style(HTML(".irs-single {color:black; background:#ffffff; font-size: 25px; font-weight: bold; font-family: 'Segoe UI', 'Source Sans Pro', Calibri, Candara, Arial, sans-serif; top: -25%}")),
-          tags$style(HTML(".irs-slider {width: 16px; height: 16px; top: 24px;}")),
-          tabItems(  # Boxes need to be put in a row (or column)
-          tabItem(tabName = "ratecompare",
-            fluidRow(
-             box(width = 12,
-             title = "Typical Monthly Electricity Usage ",
-             sliderInput("kwhslider", "Usage in kWh:", 1, 1000, 450))
-             ),
-            fluidRow(
-             box(plotOutput("plot1", height = 450)),
-             box(h2(htmlOutput("dollartext"))),
-             box(h2(htmlOutput("pcttext")))
-             ,box(h2(htmlOutput("co2avoid")))
-             ),
-            
-            # fluidRow(
-            #   box(plotOutput("plot0", height = 350)),
-            #   box(plotOutput("plot2", height = 350))
-            # ),
-            
-            fluidRow(
-             box(width = 12, title = "Electricity generation rates", 
-              splitLayout(
-               numericInput("rate5", "Default +5% renewable", value = 0.10538, step = 0.001),
-               numericInput("rate100", "Opt up 100% renewable", value = 0.13198, step = 0.001)
-               ))),
-            fluidRow(
-             box(width = 12, title = "Electricity delivery rates", 
-              splitLayout(
-               numericInput("custcharge", "Customer account charge", value = 6.43, step = 0.50),
-               numericInput("deliverycharges", "Delivery charges", value = 0.09824, step = 0.00001)
-               ))),
-             fluidRow(
-              box(width = 12, title = "CO[2] avoided by choosing renewable energy", 
-               splitLayout(
-                numericInput("co2avoid", "Pounds of CO[2] avoided per kWh of renewable energy", value = .857, step = 0.001))
-                ))
-            
-            
-              ), # End tabItem ratecompare
-        tabItem(tabName = "about",
+                      tags$style(HTML(".irs-slider {width: 16px; height: 16px; top: 24px;}")),
+                      tabItems(  # Boxes need to be put in a row (or column)
+                        tabItem(tabName = "ratecompare",
+                                fluidRow(
+                                  box(width = 12,
+                                      title = "Typical Monthly Electricity Usage ",
+                                      sliderInput("kwhslider", "Usage in kWh:", 0, 2000, 450))
+                                ),
+                                fluidRow(
+                                  box(plotOutput("plot1", height = 450)),
+                                  box(h2(htmlOutput("dollartext"))),
+                                  box(h2(htmlOutput("pcttext")))
+                                  ,box(h2(htmlOutput("co2avoid")))
+                                ),
+                                
+                                # fluidRow(
+                                #   box(plotOutput("plot0", height = 350)),
+                                #   box(plotOutput("plot2", height = 350))
+                                # ),
+                                
+                                fluidRow(
+                                  box(width = 12, title = "Electricity generation rates", 
+                                      splitLayout(
+                                        numericInput("rate5", "Default +10% renewable", value = 0.10519, step = 0.001),
+                                        numericInput("rate100", "Opt up 100% renewable", value = 0.13219, step = 0.001)
+                                      ))),
+                                fluidRow(
+                                  box(width = 12, title = "Electricity delivery rates", 
+                                      splitLayout(
+                                        numericInput("custcharge", "Customer account charge", value = 7.00, step = 0.50),
+                                        numericInput("deliverycharges", "Delivery charges", value = 0.12818, step = 0.00001)
+                                      ))),
+                                fluidRow(
+                                  box(width = 12, title = "CO[2] avoided by choosing renewable energy", 
+                                      splitLayout(
+                                        numericInput("co2avoid", "Pounds of CO[2] avoided per kWh of renewable energy", value = .857, step = 0.001))
+                                  ))
+                                
+                                
+                        ), # End tabItem ratecompare
+                        tabItem(tabName = "about",
                                 h2("About this page"),
                                 fluidPage(
                                   box(width = 12,
                                       title = "Somerville Community Choice Electricity (CCE) Aggregation",
-                                      "The Somerville Community Choice Electricity (CCE) Aggregation began July 2017.", br(), "See the", 
+                                      "This is an electricity supply program organized by the City of Somerville to provide residents and businesses with new, City-vetted options for electricity supply. The program is an important part of", a("Somerville Climate Forward", href="https://www.somervillema.gov/departments/programs/somerville-climate-forward"), "our comprehensive climate change plan. The CCE program has been running successfully since 2017.", br(), "See the", 
                                       a("Somerville CCE page", href="https://somervillecce.com"),
                                       "for more information."),
                                   
                                   box(width = 12,
                                       title = "This calculator",
-                                      "Translating the information on the", a("CCE mailer", href="https://somervilledev.files.wordpress.com/2017/12/somerville-opt-out-chart-2018.pdf"), "to your bill takes a bit of work. This program aims to acheive stable electricity pricing, increase Somerville's commitment to local renewable energy, and provide residents an option to opt up to 100% local renewable energy as their source of electricity. This calculator provides an easy way to compare how the options will affect your bill, with a goal of showing that the benefits of 100% local renewable energy can be acheived with a relatively modest increase in a household's bill.", br(),br(),
-                                      "To select the Somerville 100% Local Green option,", a("contact Dynegy at (866) 220-5696, Monday through Friday from 9AM to 8PM EST, or via email at DESCustCare@Dynegy.com", href="https://somervillecce.com/how-to-select-the-somerville-100-local-green-option/"),". You will need your Eversource account nubmer.",br(), "Alternatively, fill out the information in", a("this form on the Somerville CCE site.", href = "https://somervillecce.com/opt-up-to-premium-100-local-green/")),
+                                      "Translating the information on the", a("CCE mailer", href="https://cce.somervillema.gov/wp-content/uploads/2021/04/Somerville-Opt-Out-Letter-3-11-21.pdf"), "to your bill takes a bit of work. This program aims to acheive stable electricity pricing, increase Somerville's commitment to local renewable energy, and provide residents an option to opt up to 100% local renewable energy as their source of electricity. This calculator provides an easy way to compare how the options will affect your bill, with a goal of showing that the benefits of 100% local renewable energy can be acheived with a relatively modest increase in a household's bill.", br(),br(),
+                                      "To select the Somerville 100% Local Green option, fill out the information in", a("this form on the Somerville CCE site.", href = "https://cce.somervillema.gov/opt-up-or-join/"), "You will need your Eversource account nubmer.", br()),
                                   box(width = 12,
                                       title = "Calculating an electrical bill",
                                       "A monthly bill is calculated as follows:", br(),
                                       tags$em("kWh * Generation Rate + kWh * Delivery Rate + Customer Charge"), br(), br(),
                                       "The Generation Rate is what is being changed by the CCE plan. By opting up to 100%, only the Generation Rate aspect would increase.",
-                                      a(img(src="https://somervilledev.files.wordpress.com/2017/12/somerville-opt-out-chart-2018.pdf", width = 700, alt = "CCE Chart"), href = "https://somervilledev.files.wordpress.com/2017/12/somerville-opt-out-chart-2018.pdf"),br(),
-                                      "Delivery Rates are composed of multiple items, see your Eversource bill for details. The delivery rates shown in the calculator by default are from January 2018 for a residential customer.",
+                                      a(img(src="https://i1.wp.com/cce.somervillema.gov/wp-content/uploads/2021/04/supply-diagram.png?resize=2048%2C549&ssl=1", width = 700, alt = "CCE Chart"), href = "https://cce.somervillema.gov"),br(),
+                                      "Delivery Rates are composed of multiple items, see your Eversource bill for details. The delivery rates shown in the calculator by default are from Spring 2021 for a residential customer.",
                                       br(),
                                       "For more details on these rates, see the Eversource page on ",
-                                      a("Understanding My Electricity Bill", href="https://www.eversource.com/Content/ema-c/residential/my-account/billing-payment/understanding-my-bill", ".")
+                                      a("Understanding My Electricity Bill", href="https://www.eversource.com/content/ema-c/residential/my-account/billing-payments/about-your-bill/understanding-my-bill", ".")
                                   ), # end Calculating an electrical bill box
                                   box(width = 12,
                                       title = "Feedback",
                                       "This is a beta version, written in ",
                                       a("Shiny.", href="http://shiny.rstudio.com"),
                                       "Please leave feedback by making an Issue on", a("GitHub", href="https://github.com/flynn-d/somervillecce/"), "or by contacting dan@flynnd.io")
-                                  ) # end fluidPage
+                                ) # end fluidPage
                         ) # end tabItem about
                       ) # end tabItems
                     ) # end dashboardBody
